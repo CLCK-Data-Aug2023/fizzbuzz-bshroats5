2550489d96d34f4df202a92d104b4bff17667b7c
Bret Shroats <bshroats5@gmail.com>
2023-10-24 14:08:11 -0400

    Edited code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,10 @@
 # add your code here
-
-<<<<<<< HEAD
-# add your code here
-
-=======
->>>>>>> a04a607348fe96b711334c300970d6e040c84db5
 for i in range(1, 101):
-  if i % 3 == 0 and i % 5 == 0:
-    print("FizzBuzz")
-  elif i % 3 == 0:
-    print("Fizz")
-  elif i % 5 == 0:
-    print("Buzz")
-  else:
-<<<<<<< HEAD
-    print(i)
-=======
-    print(i)
->>>>>>> a04a607348fe96b711334c300970d6e040c84db5
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
